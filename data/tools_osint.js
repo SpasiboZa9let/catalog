93adb64850a965b911edf2bed97cc72ed3504324
Иван <foxneversleep@gmail.com>
2025-11-24 17:48:19 +0300

    Create tools_osint.js

---
+++ data/tools_osint.js
@@ -0,0 +1,22 @@
+// data/tools_osint.js
+
+export const TOOLS_OSINT = [
+  {
+    name: "Doppio Labs",
+    url: "https://www.doppio-labs.com/",
+    category: "OSINT / Поиск / Анонимность",
+    description: "Инструменты для анализа данных, кибербезопасности и OSINT."
+  },
+  {
+    name: "FilePursuit",
+    url: "https://filepursuit.com/",
+    category: "OSINT / Поиск / Анонимность",
+    description: "Поисковик файлов, архивов и ресурсов по всей сети."
+  },
+  {
+    name: "Temp Mail (Mohmal)",
+    url: "http://mohmal.com/",
+    category: "OSINT / Поиск / Анонимность",
+    description: "Временная почта для анонимных регистраций без раскрытия основного e-mail."
+  }
+];
